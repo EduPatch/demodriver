e936d28cb44fb1ef684fb481f9871a70a28f158c
ludverse <62475903+ludverse@users.noreply.github.com>
2024-10-12 20:11:21 +0200

    added example and added the context getters to the actual driver this time

--- example/demodriver_example.dart
+++ example/demodriver_example.dart
@@ -1,6 +1,34 @@
+import 'package:lib_driver/lib_driver.dart';
 import 'package:demodriver/demodriver.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+void main() async {
+  final driver = DemoDriver();
+
+  final timetablesContext = driver.getTimetableContext();
+  if (timetablesContext != null) {
+    final timetable = await timetablesContext.getTimetable(DateTime.now());
+
+    print("Printing lessons:");
+    for (var lesson in timetable.lessons) {
+      print(
+          "'${lesson.subjectName}'     teachers: ${lesson.teacherNames?.join(', ') ?? ''}");
+    }
+    print("");
+  }
+
+  final notificationsContext = driver.getNotificationsContext();
+  if (notificationsContext != null) {
+    final notifications = await notificationsContext.getNotifications();
+
+    print("Printing notifications:");
+    for (var notif in notifications) {
+      switch (notif) {
+        case final CalendarNotification calendarNotif:
+          final event = calendarNotif.event;
+		  print("Calendar event: ${event.title}");
+          break;
+        default:
+      }
+    }
+  }
 }
--- lib/src/demodriver_driver.dart
+++ lib/src/demodriver_driver.dart
@@ -1,14 +1,36 @@
+import 'package:demodriver/src/attendence_context.dart';
+import 'package:demodriver/src/calendar_context.dart';
+import 'package:demodriver/src/notifications_context.dart';
 import 'package:demodriver/src/timetable_context.dart';
 import 'package:lib_driver/lib_driver.dart';
 
 class DemoDriver extends DriverBase {
   @override
-  DriverMetadata get driverMetadata => DriverMetadata(
-      platformName: "Demo platform",
-      supportedFeatures: [ContextType.timetable]);
+  DriverMetadata get driverMetadata =>
+      DriverMetadata(platformName: "Demo platform", supportedFeatures: [
+        ContextType.timetable,
+        ContextType.calendar,
+        ContextType.notifications,
+        ContextType.attendence
+      ]);
 
   @override
   TimetableContextBase? getTimetableContext() {
     return DemoTimetableContext();
+  }
+
+  @override
+  CalendarContextBase? getCalendarContext() {
+    return DemoCalendarContext();
+  }
+
+  @override
+  NotificationsContextBase? getNotificationsContext() {
+    return DemoNotificationsContext();
+  }
+
+  @override
+  AttendenceContextBase? getAttendenceContext() {
+    return DemoAttendenceContext();
   }
 }
